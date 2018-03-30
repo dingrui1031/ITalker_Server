40ce0b641bccff51f2da280cc921e9fa108e4a72
dingrui1031 <18761903460@163.com>
2018-03-30 17:48:43 +0800

    提交AccountService代码

--- ITalker/src/main/java/net/dingrui/web/italker/push/Application.java
+++ ITalker/src/main/java/net/dingrui/web/italker/push/Application.java
@@ -1,4 +1,21 @@
 package net.dingrui.web.italker.push;
 
-public class Application {
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import net.dingrui.web.italker.push.service.AccountService;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.util.logging.Logger;
+
+/**
+ * @author dingrui
+ */
+public class Application extends ResourceConfig {
+    public Application(){
+        //注册逻辑处理的包名
+        register(AccountService.class.getPackage().getName());
+        //注册json解析器
+        register(JacksonJsonProvider.class);
+        //注册日志打印输出
+        register(Logger.class);
+    }
 }
---
+++ ITalker/src/main/java/net/dingrui/web/italker/push/service/AccountService.java
@@ -0,0 +1,31 @@
+package net.dingrui.web.italker.push.service;
+
+import net.dingrui.web.italker.push.bean.User;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * @author dingrui
+ */
+@Path("/account")
+public class AccountService {
+
+    @GET
+    @Path("/login")
+    public String get(){
+        return "You get login!";
+    }
+
+    @POST
+    //指定请求与返回的响应体
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/login")
+    public User post(){
+        User user = new User();
+        user.setName("张三");
+        user.setSex("男");
+        return user;
+    }
+}
